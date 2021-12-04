e9a405b9c01c952d63783e83385b1502a82bd6b6
Kulikov Nikita <nikita.kulikov@akvelon.com>
2021-12-04 19:07:46 +0300

    added base repository

---
+++ BLL/Repository/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using BLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Repository
+{
+    public interface IRepository<T>
+        where T : BaseEntity
+    {
+        T GetById(int id);
+        IEnumerable<T> GetAll();
+        T Create(T entity);
+        T Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ DLL/Repository/BaseRepository.cs
@@ -0,0 +1,52 @@
+﻿using BLL.Models;
+using BLL.Repository;
+using DLL.EF;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL.Repository
+{
+    public class BaseRepository<T> : IRepository<T>
+        where T : BaseEntity
+    {
+        private readonly HairShopDbContext _context;
+        protected HairShopDbContext Context => _context;
+        public BaseRepository(HairShopDbContext context)
+        {
+            _context = context;
+        }
+        public virtual T Create(T entity)
+        {
+            var entry = _context.Set<T>().Add(entity);
+            _context.SaveChanges();
+            return entry.Entity;
+        }
+
+        public virtual void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public virtual IEnumerable<T> GetAll()
+        {
+            return _context.Set<T>().AsNoTracking();
+        }
+
+        public virtual T GetById(int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+        public virtual T Update(T entity)
+        {
+            var entry = _context.Set<T>().Update(entity);
+            _context.SaveChanges();
+            return entry.Entity;
+        }
+    }
+}
